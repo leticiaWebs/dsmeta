48ec9e98a34e37d00c0f99a918ef4a5fa6e7f197
leticiaWebs <leticiaaod@gmail.com>
2022-07-14 12:01:19 -0300

    First commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,15 @@
+import NotificationButton from "./components/NotificationButon"
 
 function App() {
   return(
-    <h1>Olá mundo</h1>
+    <>
+    <h1>Olá Mundo!</h1>
+    <NotificationButton/>
+    <NotificationButton/>
+    <NotificationButton/>
+    <NotificationButton/>
+    <NotificationButton/>
+    </>
   )
 }
 
